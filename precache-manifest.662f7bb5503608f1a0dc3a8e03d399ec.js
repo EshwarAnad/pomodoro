17903d37e5fa6d61b093f07b69cf17a9cfb0e28c
Carrie <chanscom7@gmail.com>
2020-09-18 12:52:51 -0400

    Updates

---
+++ precache-manifest.662f7bb5503608f1a0dc3a8e03d399ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f56fd6c0a3f1f306fb8f764124bd19f3",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "c037656a762020e785ec",
+    "url": "/pomodoro/static/css/main.cb225e29.chunk.css"
+  },
+  {
+    "revision": "6bf7a0522eb6ec9f2e3b",
+    "url": "/pomodoro/static/js/2.5b438290.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro/static/js/2.5b438290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c037656a762020e785ec",
+    "url": "/pomodoro/static/js/main.22ce379b.chunk.js"
+  },
+  {
+    "revision": "ee3590aa5d45d2dacf12",
+    "url": "/pomodoro/static/js/runtime-main.f3456f13.js"
+  }
+]);
